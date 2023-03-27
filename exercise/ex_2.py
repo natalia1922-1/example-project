e179272efb9d3e3b315553ec6277bdb786e730b3
natalianowicka <natalia.nowicka@opoczta.pl>
2023-03-27 12:41:45 +0200

    dodanie prostokata

--- exercise/ex_2.py
+++ exercise/ex_2.py
@@ -8,6 +8,14 @@ pole = a * a
 
 print("Obwod kwadratu wynosi " + str(obwod) + ", a pole " + str(pole) + ".")
 
+# obwod kwadratu wynosi 40, a pole 100.
 
-#obwod kwadratu wynosi 40, a pole 100.
+# prostokat
 
+b = 5
+
+obwod_p = 2 * a + 2 * b
+
+pole_p = a * b
+
+print("Obwod prostokata wynosi " + str(obwod_p) + ", a pole " + str(pole_p) + ".")
