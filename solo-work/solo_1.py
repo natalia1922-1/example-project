20ff937ab4439e6c8f99239a93c2fb342db81a2c
natalianowicka <natalia.nowicka@opoczta.pl>
2023-04-17 21:25:31 +0200

    zadanie 1.2

--- solo-work/solo_1.py
+++ solo-work/solo_1.py
@@ -1,7 +1,10 @@
 # zadanie 1.1
 
-hello = "Hello"
-student = "Ola"
-print("{} {}".format(hello, student))
+# hello = "Hello"
+# student = "Ola"
+# print("{} {}".format(hello, student))
 
+# zadanie 1.2
 
+student = input("Wpisz swoje imie ")
+print("Hello", student)
