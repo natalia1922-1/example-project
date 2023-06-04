01901896cfbe9623249c74c71a3430b0c191a862
natalianowicka <natalia.nowicka@opoczta.pl>
2023-06-05 00:57:20 +0200

    zadanie domowe - solo_3.py

--- solo-work/salary.py
+++ solo-work/salary.py
@@ -1,31 +1,66 @@
 import csv
 
+
 class Pracownik:
-    def __init__(self, imie, nazwisko, wynagrodzenie_brutto, procent_skladki_wypadkowej=0.03):
+    def __init__(self, imie, nazwisko, wynagrodzenie_brutto):
         self.imie = imie
         self.nazwisko = nazwisko
-        self.wynagrodzenie_brutto = wynagrodzenie_brutto
-        self.procent_skladki_wypadkowej = procent_skladki_wypadkowej
+        self.wynagrodzenie_brutto = float(wynagrodzenie_brutto)
+        self.skladka_rentowa_prcw = self.wynagrodzenie_brutto * 0.015
+        self.skladka_emerytalna_prcw = self.wynagrodzenie_brutto * 0.0976
+        self.skladka_chorobowa = self.wynagrodzenie_brutto * 0.0245
+        # self.skladka_zdrowotna = (self.wynagrodzenie_brutto - (self.skladka_rentowa_prcw + self.skladka_emerytalna_prcw
+        #                                                        + self.skladka_chorobowa)) * 0.09
+        self.skladka_zdrowotna = 0.09
+        self.koszty_uzysk_przych = 250
+        self.kwota_wolna = 300
 
     def __str__(self):
         pass
 
-    def oblicz_netto(self):
-        if (self.składka_zdrowotna == ""):
-            return
+    def set_koszty_uzysk_przych(self, koszty):
+        if (koszty == 0 or koszty == 250 or koszty == 300):
+            self.koszty_uzysk_przych = koszty
+        else:
+            print("Koszty uzyskanych przychodow moga .... 0, 250 lub 300, ustawiono na 250")
+            self.koszty_uzysk_przych = 250
+
+    def set_kwota_wolna(self, kwota):
+        if (kwota == 0 or kwota == 100 or kwota == 150 or kwota == 300):
+            self.kwota_wolna = kwota
+        else:
+            print("kwota wolna może wynosić 0, 100, 150, lub 300, ustawiono na wartość 300")
+            self.kwota_wolna = 300
+
+    def oblicz_koszty_pracownika(self):
+        koszty_pracownik = self.skladka_rentowa_prcw + self.skladka_emerytalna_prcw + self.skladka_zdrowotna + self.skladka_chorobowa
+        return koszty_pracownik
 
     def oblicz_koszty_pracodawcy(self):
-        skladka_rentowa = self.wynagrodzenie_brutto * 0.015
-        skladka_emerytalna = self.wynagrodzenie_brutto * 0.0976
+        skladka_rentowa_prcd = self.wynagrodzenie_brutto * 0.065
+        skladka_emerytalna_prcd = self.wynagrodzenie_brutto * 0.0976
         fundusz_pracy = self.wynagrodzenie_brutto * 0.0245
-        skladka_wypadkowa = self.wynagrodzenie_brutto * self.procent_skladki_wypadkowej
-        FGŚP = self.wynagrodzenie_brutto * 0.001
-        koszty_pracodawcy = skladka_rentowa + skladka_emerytalna + fundusz_pracy + FGŚP + skladka_wypadkowa
+        skladka_wypadkowa = self.wynagrodzenie_brutto * 0.0167
+        fgsp = self.wynagrodzenie_brutto * 0.001
+        koszty_pracodawcy = skladka_rentowa_prcd + skladka_emerytalna_prcd + fundusz_pracy + fgsp + skladka_wypadkowa
         return koszty_pracodawcy
 
+    def oblicz_netto(self):
+
+        ubezpieczenie_zdrowotne = (self.wynagrodzenie_brutto - (self.skladka_rentowa_prcw +
+                                                                self.skladka_emerytalna_prcw +
+                                                                self.skladka_chorobowa)) * self.skladka_zdrowotna
+
+        podstawa_opod = (self.wynagrodzenie_brutto - (self.skladka_rentowa_prcw + self.skladka_emerytalna_prcw +
+                                                      self.skladka_chorobowa)) - self.koszty_uzysk_przych
+
+        zaliczka_pod_doch = (podstawa_opod * 0.12) - self.kwota_wolna
+
+        koszty_pracownik = self.oblicz_koszty_pracownika()
+        return self.wynagrodzenie_brutto - koszty_pracownik - ubezpieczenie_zdrowotne - zaliczka_pod_doch
 
 
-pracownicy = [] # z csv
+pracownicy = []  # z csv
 with open('salary.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
@@ -36,15 +71,31 @@ with open('salary.csv') as csv_file:
             pracownik = Pracownik(row[0], row[1], row[2])
             pracownicy.append(pracownik)
 
-# for pracownik in_pracownicy:
-#     print("Pracownik Kowalski: ")
-#     print("- prensja brutto: ")
-#     print("- pensja netto: ")
-#     print("- koszty pracodawcy: ")
-#     print("- koszt całkowity: ")
+
+
+pracownicy[0].set_koszty_uzysk_przych(250)
+pracownicy[1].set_koszty_uzysk_przych(250)
+pracownicy[2].set_koszty_uzysk_przych(250)
+pracownicy[3].set_koszty_uzysk_przych(0)
+pracownicy[4].set_koszty_uzysk_przych(300)
+pracownicy[5].set_koszty_uzysk_przych(0)
+
+pracownicy[0].set_kwota_wolna(0)
+pracownicy[1].set_kwota_wolna(0)
+pracownicy[2].set_kwota_wolna(300)
+pracownicy[3].set_kwota_wolna(150)
+pracownicy[4].set_kwota_wolna(100)
+pracownicy[5].set_kwota_wolna(300)
+
+
+for pracownik in pracownicy:
+    print("Pracownik :", pracownik.imie, pracownik.nazwisko)
+    print("- prensja brutto: ", pracownik.wynagrodzenie_brutto)
+    print("- pensja netto: ", pracownik.oblicz_netto())
+    print("- koszty pracodawcy: ", pracownik.oblicz_koszty_pracodawcy())
+    print("- koszt całkowity: ")
 
 # print("Suma kosztów wynosi: xxx")
 
 # zdrowotna, emerytalna, rentowna - koszty pracownika
 # koszty pracodawcy -
-
